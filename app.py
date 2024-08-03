fb5b282d0da7f87a7e98a1bcaadf77bd9b22a8c2
Rahul Gayke <rahulsgayke@gmail.com>
2024-08-04 01:00:40 +0530

    updated train.py for mlflow loggings

--- app.py
+++ app.py
@@ -1,52 +1,40 @@
 from flask import Flask, render_template, session, redirect, url_for, session
 from flask_wtf import FlaskForm
-from wtforms import TextField,SubmitField
+from wtforms import TextField, SubmitField
 from wtforms.validators import NumberRange
 import numpy as np  
 import joblib
 
-
-
-def return_prediction(rf_random_1,rf_random_2,scaler_x,scaler_y1,scaler_y2,sample_json):
+def return_prediction(rf_1, rf_2, scaler_x, scaler_y1, scaler_y2, sample_json):
+    # Fields for input json
+    Relative_compactness = sample_json['Relative_compactness']
+    Surface_area = sample_json['Surface_area']
+    Wall_area = sample_json['Wall_area']
+    Roof_area = sample_json['Roof_area']
+    Overall_height = sample_json['Overall_height']
+    Orientation = sample_json['Orientation']
+    Glazing_area = sample_json['Glazing_area']
+    Glazing_area_distribution = sample_json['Glazing_area_distribution']
     
-    # For larger data features, you should probably write a for loop
-    # That builds out this array for you
-
-    Relative_compactness=sample_json['Relative_compactness']
-    Surface_area=sample_json['Surface_area']
-    Wall_area=sample_json['Wall_area']
-    Roof_area=sample_json['Roof_area']
-    Overall_height=sample_json['Overall_height']
-    Orientation=sample_json['Orientation']
-    Glazing_area=sample_json['Glazing_area']
-    Glazing_area_distribution=sample_json['Glazing_area_distribution']
-
-
-
-
-    
-    sample = [[Relative_compactness,Surface_area,Wall_area,Roof_area,Overall_height,Orientation,Glazing_area,Glazing_area_distribution]]
+    sample = [[Relative_compactness, Surface_area, Wall_area, Roof_area, Overall_height, Orientation, Glazing_area, Glazing_area_distribution]]
     sample = scaler_x.transform(sample)
     
+    Heating_Load = rf_1.predict(sample)
+    Heating_Load = scaler_y1.inverse_transform(Heating_Load.reshape(-1, 1))
     
-    Heating_Load = rf_random_1.predict(sample)
-    Heating_Load=scaler_y1.inverse_transform(Heating_Load.reshape(-1, 1))
+    Cooling_Load = rf_2.predict(sample)
+    Cooling_Load = scaler_y2.inverse_transform(Cooling_Load.reshape(-1, 1))
     
-    Cooling_Load= rf_random_2.predict(sample)
-    Cooling_Load=scaler_y2.inverse_transform(Cooling_Load.reshape(-1, 1))
-    
-    return Heating_Load,Cooling_Load
-
-
+    return Heating_Load, Cooling_Load
 
 app = Flask(__name__)
+
 # Configure a secret SECRET_KEY
 app.config['SECRET_KEY'] = 'mysecretkey'
 
-
 # LOAD THE MODEL AND THE SCALERS
-rf_random_1 = joblib.load("./models/rf_random_1.joblib")
-rf_random_2 = joblib.load("./models/rf_random_2.joblib")
+rf_1 = joblib.load("./models/rf_1.joblib")
+rf_2 = joblib.load("./models/rf_2.joblib")
 scaler_x = joblib.load("./models/scaler_x.pkl")
 scaler_y1 = joblib.load("./models/scaler_y1.pkl")
 scaler_y2 = joblib.load("./models/scaler_y2.pkl")
@@ -56,7 +44,7 @@ scaler_y2 = joblib.load("./models/scaler_y2.pkl")
 # WTForm Class
 # Lots of fields available: http://wtforms.readthedocs.io/en/stable/fields.html
 class FlowerForm(FlaskForm):
-    
+
     Relative_compactness = TextField('Relative compactness')
     Surface_area = TextField('Surface area')
     Wall_area = TextField('Wall area')
@@ -67,7 +55,6 @@ class FlowerForm(FlaskForm):
     Glazing_area_distribution = TextField('Glazing area distribution')
 
     submit = SubmitField('Predict')
-
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -90,7 +77,6 @@ def index():
 
         return redirect(url_for("prediction"))
 
-
     return render_template('home.html', form=form)
 
 
@@ -108,11 +94,9 @@ def prediction():
     content['Glazing_area'] = float(session['Glazing_area'])
     content['Glazing_area_distribution'] = float(session['Glazing_area_distribution'])
 
-    results = return_prediction(rf_random_1=rf_random_1,rf_random_2=rf_random_2,scaler_x=scaler_x,scaler_y1=scaler_y1,scaler_y2=scaler_y2,sample_json=content)
-    
+    results = return_prediction(rf_random_1=rf_random_1, rf_random_2=rf_random_2, scaler_x=scaler_x, scaler_y1=scaler_y1, scaler_y2=scaler_y2, sample_json=content)
 
-    return render_template('prediction.html',results=results)
-
+    return render_template('prediction.html', results=results)
 
 if __name__ == '__main__':
     app.run(host="0.0.0.0",port=8080)
--- train.py
+++ train.py
@@ -1,6 +1,5 @@
-#%% This code snippet is to train and save the machine learning models
+# This code snippet is to train and save the machine learning models
 # The datset was extracted from https://archive.ics.uci.edu/ml/datasets/energy+efficiency
-
 import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
@@ -12,126 +11,187 @@ from sklearn.model_selection import train_test_split
 from sklearn.preprocessing import MinMaxScaler
 from sklearn.metrics import r2_score
 import joblib
-###############################################################################################################################
-#%% load the data
-df=pd.read_csv('./data/ENB2012_CSV_data.csv')
+import mlflow
+import mlflow.sklearn
+import argparse
+from sklearn.tree import DecisionTreeRegressor
 
-# Naming the columns based on the documentation from machine learning repository (link above)
-for col in df.columns:
-    if col=='Y1':
-        df.rename(columns={col:'Heating_Load'}, inplace=True)
-    if col=='Y2':
-        df.rename(columns={col:'Cooling_Load'}, inplace=True)
-    if col=='X1':
-        df.rename(columns={col:'Relative_compactness'}, inplace=True)
-    if col=='X2':
-        df.rename(columns={col:'Surface_area'}, inplace=True)
-    if col=='X3':
-        df.rename(columns={col:'Wall_area'}, inplace=True)
-    if col=='X4':
-        df.rename(columns={col:'Roof_area'}, inplace=True)
-    if col=='X5':
-        df.rename(columns={col:'Overall_height'}, inplace=True)
-    if col=='X6':
-        df.rename(columns={col:'Orientation'}, inplace=True)
-    if col=='X7':
-        df.rename(columns={col:'Glazing_area'}, inplace=True)
-    if col=='X8':
-        df.rename(columns={col:'Glazing_area_distribution'}, inplace=True)
+def load_data(path):
+    df = pd.read_csv(path)
+    # Naming the columns based on the documentation from machine learning repository (link above)
+    for col in df.columns:
+        if col=='Y1':
+            df.rename(columns={col:'Heating_Load'}, inplace=True)
+        if col=='Y2':
+            df.rename(columns={col:'Cooling_Load'}, inplace=True)
+        if col=='X1':
+            df.rename(columns={col:'Relative_compactness'}, inplace=True)
+        if col=='X2':
+            df.rename(columns={col:'Surface_area'}, inplace=True)
+        if col=='X3':
+            df.rename(columns={col:'Wall_area'}, inplace=True)
+        if col=='X4':
+            df.rename(columns={col:'Roof_area'}, inplace=True)
+        if col=='X5':
+            df.rename(columns={col:'Overall_height'}, inplace=True)
+        if col=='X6':
+            df.rename(columns={col:'Orientation'}, inplace=True)
+        if col=='X7':
+            df.rename(columns={col:'Glazing_area'}, inplace=True)
+        if col=='X8':
+            df.rename(columns={col:'Glazing_area_distribution'}, inplace=True)
+    return df
 
-###############################################################################################################################
-#%% train/test split and normlization of data
-X=df.drop(["Heating_Load","Cooling_Load"],axis=1)
-y1=df['Heating_Load']
-y3=df['Cooling_Load'] # we want to use y3 to geenrate y2
+def split_datset(df):
+    X = df.drop(["Heating_Load","Cooling_Load"], axis=1)
+    y1 = df['Heating_Load']
+    y3 = df['Cooling_Load'] # we want to use y3 to geenrate y2
 
-# X_train will be used for model building and parametrs tunning and X_test is used for testing the model
-X_train,X_test,y_train_1,y_test_1=train_test_split(X,y1,test_size=0.2,random_state=4)
-y_train_2=y3[y_train_1.index]
-y_test_2=y3[y_test_1.index]
+    # X_train will be used for model building and parametrs tunning and X_test is used for testing the model
+    X_train, X_test, y_train_1, y_test_1 = train_test_split(X, y1, test_size=0.2, random_state=4)
+    y_train_2 = y3[y_train_1.index]
+    y_test_2 = y3[y_test_1.index]
 
-# scaling the input data
-scaler_x = MinMaxScaler()
-scaler_x.fit(X_train)
-scaled_X_train=scaler_x.transform(X_train)
-scaled_X_test = scaler_x.transform(X_test)
+    return X_train, X_test, y_train_1, y_test_1, y_train_2, y_test_2
 
-# scaling the output data
-scaler_y1=MinMaxScaler()
-scaler_y1.fit(y_train_1.values.reshape(-1, 1))
-scaled_y_train_1=scaler_y1.transform(y_train_1.values.reshape(-1, 1))
-scaled_y_train_1=scaled_y_train_1.ravel()
-scaled_y_test_1 = scaler_y1.transform(y_test_1.values.reshape(-1, 1))
-scaled_y_test_1=scaled_y_test_1.ravel()
+def scale_data(X_train, X_test, y_train_1, y_test_1, y_train_2, y_test_2):
+    # scaling the input data
+    scaler_x = MinMaxScaler()
+    scaler_x.fit(X_train)
+    scaled_X_train = scaler_x.transform(X_train)
+    scaled_X_test = scaler_x.transform(X_test)
 
-scaler_y2=MinMaxScaler()
-scaler_y2.fit(y_train_2.values.reshape(-1, 1))
-scaled_y_train_2=scaler_y2.transform(y_train_2.values.reshape(-1, 1))
-scaled_y_train_2=scaled_y_train_2.ravel()
-scaled_y_test_2 = scaler_y2.transform(y_test_2.values.reshape(-1, 1))
-scaled_y_test_2=scaled_y_test_2.ravel()
+    # scaling the output data
+    scaler_y1 = MinMaxScaler()
+    scaler_y1.fit(y_train_1.values.reshape(-1, 1))
+    scaled_y_train_1 = scaler_y1.transform(y_train_1.values.reshape(-1, 1))
+    scaled_y_train_1 = scaled_y_train_1.ravel()
+    scaled_y_test_1 = scaler_y1.transform(y_test_1.values.reshape(-1, 1))
+    scaled_y_test_1 = scaled_y_test_1.ravel()
 
+    scaler_y2 = MinMaxScaler()
+    scaler_y2.fit(y_train_2.values.reshape(-1, 1))
+    scaled_y_train_2 = scaler_y2.transform(y_train_2.values.reshape(-1, 1))
+    scaled_y_train_2 = scaled_y_train_2.ravel()
+    scaled_y_test_2 = scaler_y2.transform(y_test_2.values.reshape(-1, 1))
+    scaled_y_test_2 = scaled_y_test_2.ravel()
 
-###############################################################################################################################
-#%% Training the random forest model using RandomizedSearchCV for optimization of parameters
+    return scaled_X_train, scaled_y_train_1, scaled_y_train_2, scaled_X_test, scaler_y1, scaler_y2, scaler_x
 
-# Number of trees in random forest
-n_estimators = [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)]
-# Number of features to consider at every split
-max_features = ['auto', 'sqrt']
-# Maximum number of levels in tree
-max_depth = [int(x) for x in np.linspace(10, 110, num = 11)]
-max_depth.append(None)
-# Minimum number of samples required to split a node
-min_samples_split = [2, 5, 10]
-# Minimum number of samples required at each leaf node
-min_samples_leaf = [1, 2, 4]
-# Method of selecting samples for training each tree
-bootstrap = [True, False]
-# Create the random grid
-random_grid = {'n_estimators': n_estimators,
-           'max_features': max_features,
-           'max_depth': max_depth,
-           'min_samples_split': min_samples_split,
-           'min_samples_leaf': min_samples_leaf,
-           'bootstrap': bootstrap}
+def train_rf_regressor(scaled_X_train, scaled_y_train_1, scaled_y_train_2, params_dict):
+    # Heating Load
+    n_estimators = params_dict["n_estimators"]
+    max_depth = params_dict["max_depth"]
+    min_samples_split = params_dict["min_samples_split"]
 
-# Heating Load
-rf_1 = RandomForestRegressor() #n_estimators = 1000, random_state = 42
-rf_random_1 = RandomizedSearchCV(estimator = rf_1, param_distributions = random_grid, n_iter = 100, cv = 10, verbose=2, random_state=42, n_jobs = -1)
-rf_random_1.fit(scaled_X_train, scaled_y_train_1)
+    rf_1 = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth, min_samples_split=min_samples_split) #n_estimators = 1000, random_state = 42
+    # rf_random_1 = RandomizedSearchCV(estimator = rf_1, param_distributions = random_grid, n_iter = 100, cv = 10, verbose=2, random_state=42, n_jobs = -1)
+    rf_1.fit(scaled_X_train, scaled_y_train_1)
 
-# Cooling Load
-rf_2 = RandomForestRegressor() #n_estimators = 1000, random_state = 42
-rf_random_2 = RandomizedSearchCV(estimator = rf_2, param_distributions = random_grid, n_iter = 100, cv = 10, verbose=2, random_state=42, n_jobs = -1)
-rf_random_2.fit(scaled_X_train, scaled_y_train_2)
-###############################################################################################################################
-#%% model evaluation
+    # Cooling Load
+    rf_2 = RandomForestRegressor(n_estimators=n_estimators, max_depth=max_depth, min_samples_split=min_samples_split) #n_estimators = 1000, random_state = 42
+    # rf_random_2 = RandomizedSearchCV(estimator = rf_2, param_distributions = random_grid, n_iter = 100, cv = 10, verbose=2, random_state=42, n_jobs = -1)
+    rf_2.fit(scaled_X_train, scaled_y_train_2)
 
-y_pred1=rf_random_1.predict(scaled_X_test)
-y_pred1=scaler_y1.inverse_transform(y_pred1.reshape(-1, 1))
-y_pred2=rf_random_2.predict(scaled_X_test)
-y_pred2=scaler_y2.inverse_transform(y_pred2.reshape(-1, 1))
-###############################################################################################################################
-#%% print the results of evaluation        
+    return rf_1, rf_2
 
+def eveluate_model(rf_1, scaled_X_test, scaler_y1, rf_2, scaler_y2):
+    y_pred1 = rf_1.predict(scaled_X_test)
+    y_pred1 = scaler_y1.inverse_transform(y_pred1.reshape(-1, 1))
+    y_pred2 = rf_2.predict(scaled_X_test)
+    y_pred2 = scaler_y2.inverse_transform(y_pred2.reshape(-1, 1))
 
-print("#### Heating Load Models were trained and tested with average r2 score of {:0.2f} %".format(r2_score(y_test_1,y_pred1)*100))
-print("")
-print("#### Cooling Load Models were trained and tested with average r2 score of {:0.2f} %".format(r2_score(y_test_2,y_pred2)*100))
+    return y_pred1, y_pred2
 
+def dump_scalers(scaler_x, scaler_y1, scaler_y2):
+    # scalers
+    joblib.dump(scaler_x,"./models/scaler_x.pkl")
+    joblib.dump(scaler_y1,"./models/scaler_y1.pkl")
+    joblib.dump(scaler_y2,"./models/scaler_y2.pkl")
 
-###############################################################################################################################
-#%% save the models
+def dump_models(rf_1, rf_2):
+    # models
+    joblib.dump(rf_1, "./models/rf_1.joblib")
+    joblib.dump(rf_2, "./models/rf_2.joblib")
 
-# scalers
-joblib.dump(scaler_x,"./models/scaler_x.pkl")
-joblib.dump(scaler_y1,"./models/scaler_y1.pkl")
-joblib.dump(scaler_y2,"./models/scaler_y2.pkl")
-# models
-joblib.dump(rf_random_1, "./models/rf_random_1.joblib")
-joblib.dump(rf_random_2, "./models/rf_random_2.joblib")
+def main(n_estimators, max_depth, min_samples_split):
+    # load the data\
+    path = "./data/ENB2012_CSV_data.csv"
+    df = load_data(path)
 
+    # train/test split
+    X_train, X_test, y_train_1, y_test_1, y_train_2, y_test_2 = split_datset(df)
 
+    # Scaling splitted data
+    scaled_X_train, scaled_y_train_1, scaled_y_train_2, scaled_X_test, scaler_y1, scaler_y2, scaler_x = scale_data(X_train, X_test, y_train_1, y_test_1, y_train_2, y_test_2)
+
+    # Create the random grid
+    # random_grid = {
+    #     # Number of trees in random forest
+    #     "n_estimators" : [int(x) for x in np.linspace(start = 200, stop = 2000, num = 10)],
+    #     # Number of features to consider at every split
+    #     "max_features" : ['auto', 'sqrt'],
+    #     # Maximum number of levels in tree
+    #     "max_depth" : [int(x) for x in np.linspace(10, 110, num = 11)], 
+    #     # Minimum number of samples required to split a node
+    #     "min_samples_split" : [2, 5, 10],
+    #     # Minimum number of samples required at each leaf node
+    #     "min_samples_leaf" : [1, 2, 4],
+    #     # Method of selecting samples for training each tree
+    #     "bootstrap" : [True, False],
+    # }
+    params_dict = {
+        "n_estimators" : n_estimators,
+        "max_depth" : max_depth,
+        "min_samples_split" : min_samples_split,
+    }
+
+    # Training the random forest model using RandomizedSearchCV for optimization of parameters
+    rf_1, rf_2 = train_rf_regressor(scaled_X_train, scaled_y_train_1, scaled_y_train_2, params_dict)
+
+    # model evaluation
+    y_pred1, y_pred2 = eveluate_model(rf_1, scaled_X_test, scaler_y1, rf_2, scaler_y2)
+    r2_score_rf1 = r2_score(y_test_1,y_pred1)*100
+    r2_score_rf2 = r2_score(y_test_2,y_pred2)*100
+
+    # print the results of evaluation        
+    print("#### Heating Load Models were trained and tested with average r2 score of {:0.2f} %".format(r2_score_rf1))
+    print("")
+    print("#### Cooling Load Models were trained and tested with average r2 score of {:0.2f} %".format(r2_score_rf2))
+
+    # save the models
+    dump_scalers(scaler_x, scaler_y1, scaler_y2)
+    dump_models(rf_1, rf_2)
+
+    print("Done Model Training")
+
+    # Logging parameters
+    mlflow.log_param("n_estimators", n_estimators)
+    mlflow.log_param("max_depth", max_depth)
+    mlflow.log_param("min_samples_split",  min_samples_split)
+
+    # Logging Metric
+    # mlflow.log_metric(r2_score_rf1, "r2_score for heating load model")
+    # mlflow.log_metric(r2_score_rf2, "r2_score for cooling load model")
+    mlflow.log_metric("r2_score for heating load model", r2_score_rf1)
+    mlflow.log_metric("r2_score for cooling load model", r2_score_rf2)
+
+    # Logging model
+    mlflow.sklearn.log_model(rf_1, "model for heating load")
+    mlflow.sklearn.log_model(rf_2, "model for Cooling load")
+
+    print("Completed mlflow loggings")
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+
+    parser.add_argument("--n_estimators", type=int, default=1000)
+    parser.add_argument("--max_depth", type=int, default=10)
+    parser.add_argument("--min_samples_split", type=int, default=5)
+
+    args = parser.parse_args()
+
+    with mlflow.start_run():
+        main(args.n_estimators, args.max_depth, args.min_samples_split)
 
 
